93f8dc3b2bcdae959b5c182cb6379c0b6199b320
Alvina <alvinadr24@gmail.com>
2026-06-03 21:56:41 +0200

    init commit

---
+++ js/scrolling-text.js
@@ -0,0 +1,13 @@
+var html = `
+<link rel="stylesheet" href="../css/scroll-text.css">
+<div style="width: 10000000px">
+    <div class="scroll-text" >
+        <h5>&nbsp;NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> </h5>
+    </div>
+    <div class="scroll-text">
+        <h5>&nbsp;NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> </h5>
+    </div>
+</div>
+` 
+
+document.getElementById("scrolling-text").innerHTML = html;
