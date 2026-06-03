29835d7f9f4e689259c0a48332ef71a7f8f68c05
Alvina <alvinadr24@gmail.com>
2026-06-03 22:07:42 +0200

    fix

--- js/scrolling-text.js
+++ js/scrolling-text.js
@@ -1,5 +1,5 @@
 var html = `
-<link rel="stylesheet" href="css/scroll-text.css">
+<link rel="stylesheet" href="./css/scroll-text.css">
 <div style="width: 10000000px">
     <div class="scroll-text" >
         <h5>&nbsp;NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> NO TOAAA <span>🩷❤️</span> NO TOAAA <span>❤️🩷</span> </h5>
